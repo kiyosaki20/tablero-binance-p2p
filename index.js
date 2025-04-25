2ffaf1940b1025edb163f619e1c84968f0d8ce20
kiyosaki20 <34486151+kiyosaki20@users.noreply.github.com>
2025-04-25 14:25:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TableroBinanceP2P from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <TableroBinanceP2P />
+  </React.StrictMode>
+);
